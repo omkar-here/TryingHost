96ea9ff53810f7156664d356fdea7d814360f13b
omkar-here <aomkarg2003@gmail.com>
2023-04-28 19:39:49 +0530

    Updates

---
+++ precache-manifest.51d6599868fc7892dddce1323acad9d6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad74cf0f2c06c7f5492d7f920401bab1",
+    "url": "/jeev-rakshak/index.html"
+  },
+  {
+    "revision": "f4d539651a98e2931ecb",
+    "url": "/jeev-rakshak/static/css/2.e9c0e2c3.chunk.css"
+  },
+  {
+    "revision": "ed0db00739c79c2d7231",
+    "url": "/jeev-rakshak/static/css/main.095347a0.chunk.css"
+  },
+  {
+    "revision": "f4d539651a98e2931ecb",
+    "url": "/jeev-rakshak/static/js/2.a62c4bd9.chunk.js"
+  },
+  {
+    "revision": "18f398058a43d533461c87f77b593a52",
+    "url": "/jeev-rakshak/static/js/2.a62c4bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0db00739c79c2d7231",
+    "url": "/jeev-rakshak/static/js/main.652f3280.chunk.js"
+  },
+  {
+    "revision": "afdbb80fb1080b2c6062",
+    "url": "/jeev-rakshak/static/js/runtime-main.5e2d25b1.js"
+  },
+  {
+    "revision": "554d3bd329b1d6cda0d8d750f97df234",
+    "url": "/jeev-rakshak/static/media/JeevRakshak.554d3bd3.png"
+  },
+  {
+    "revision": "2e1ac5095902ce2f243963f0f27c5598",
+    "url": "/jeev-rakshak/static/media/MobileNet.2e1ac509.png"
+  },
+  {
+    "revision": "c01704da80cbafacd0a186cae8e06fb4",
+    "url": "/jeev-rakshak/static/media/MobileNetSSD.c01704da.png"
+  },
+  {
+    "revision": "26b4ca378d987e6b96a208b480402853",
+    "url": "/jeev-rakshak/static/media/SSD.26b4ca37.png"
+  },
+  {
+    "revision": "41c8fe9005f22e22499e7650e6c66615",
+    "url": "/jeev-rakshak/static/media/Species-Identification.41c8fe90.png"
+  },
+  {
+    "revision": "e861cd636f1b3eedcbc20ab67093e43c",
+    "url": "/jeev-rakshak/static/media/accuracy.e861cd63.png"
+  },
+  {
+    "revision": "d4c12f3557fdef5cc99fa596e1bad874",
+    "url": "/jeev-rakshak/static/media/animal-spotting.d4c12f35.png"
+  },
+  {
+    "revision": "6e8e971b0ab89ebd956fd9fbf10c55fe",
+    "url": "/jeev-rakshak/static/media/ease-of-use.6e8e971b.png"
+  },
+  {
+    "revision": "aba3531c54d2a85939da13dda7b93350",
+    "url": "/jeev-rakshak/static/media/keras.aba3531c.png"
+  },
+  {
+    "revision": "e1099df75788b29bcffd8bbdafd3647f",
+    "url": "/jeev-rakshak/static/media/react.e1099df7.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/jeev-rakshak/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/jeev-rakshak/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/jeev-rakshak/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "3b9f1aff393dabfa538fcd722b492021",
+    "url": "/jeev-rakshak/static/media/saves-manual-labour.3b9f1aff.png"
+  },
+  {
+    "revision": "0a166aba558774c4ab4faf61a235a7d4",
+    "url": "/jeev-rakshak/static/media/species-identification.0a166aba.png"
+  },
+  {
+    "revision": "df661c755b80475d89b5191f05b72dd1",
+    "url": "/jeev-rakshak/static/media/tf.df661c75.png"
+  }
+]);
